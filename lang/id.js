20ffbb3dedcf9981bdb682ffe073105044d7528e
Wiktor Walc <w.walc@cksource.com>
2013-07-17 17:38:41 +0200

    Updated translations, added Indonesian language.

--- lang/id.js
+++ lang/id.js
@@ -2,7 +2,7 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'maximize', 'ko', {
-	maximize: '최대',
-	minimize: '최소'
+CKEDITOR.plugins.setLang( 'maximize', 'id', {
+	maximize: 'Memperbesar',
+	minimize: 'Memperkecil'
 });
